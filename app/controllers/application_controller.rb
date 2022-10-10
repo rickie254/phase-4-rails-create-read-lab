83ef1837d0f60138baac768ed4619bb70098a1c4
rickie <rickiemaweu@gmail.com>
2022-10-11 00:41:22 +0300

    done

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,19 @@
-class ApplicationController < ActionController::API
-  include ActionController::Cookies
+class PlantsController < ApplicationController
+  def index
+      render json: Plant.all
+  end
 
+  def show
+      render json: Plant.find_by(id:params[:id])
+  end
+
+  def create
+      render json: Plant.create(plant_params), status: :created
+      end
+
+      private
+
+      def plant_params
+          params.permit(:name, :image, :price)
+          end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,9 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_09_30_212306) do
+ActiveRecord::Schema.define(version: 2022_09_30_221008) do
 
   create_table "plants", force: :cascade do |t|
-    t.string "no"
-    t.string "framework"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
